ebece2d491cdf9198915ed735b85462f294ad46b
alshafi834 <alshafi834@gmail.com>
2023-03-31 19:20:28 +0600

    final push

--- cronscript.py
+++ cronscript.py
@@ -1,40 +1,35 @@
+#import necessary packages
 import os
 import csv
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = 'https://howis.eglv.de/pegel/html/uebersicht_internet.php'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+url = 'https://howis.eglv.de/pegel/html/uebersicht_internet.php' # define the URL
+response = requests.get(url) # sending the request to the url
+soup = BeautifulSoup(response.content, 'html.parser') # scrapping the html content with beautifulsoup
 
-tooltip_divs = soup.select('div.tooltip')
-tooltip_heads = []
-id_counter = 1
+tooltip_divs = soup.select('div.tooltip') # selecting all the div with class 'tooltip'
+tooltip_heads = [] # defining an array
+id_counter = 1 # defining a counter flag
 
-# for div in tooltip_divs:
-#     tooltip_head = div.select_one('.tooltip-head').text.replace('\n', '')
-#     tooltip_head_data = ['s'+str(id_counter), tooltip_head, datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-#     tooltip_values = div.select('td.tooltip-value')
-#     for value in tooltip_values:
-#         tooltip_head_data.append(value.text.replace('\xa0', ''))
-#     tooltip_heads.append(tooltip_head_data)
-#     id_counter += 1
-
+# looping through the tooltip divs
 for div in tooltip_divs:
-    tooltip_head = div.select_one('.tooltip-head').text.replace('\n', '')
-    tooltip_head_data = ['s'+str(id_counter), tooltip_head, datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-    tooltip_values = div.select('td.tooltip-value')
+    tooltip_head = div.select_one('.tooltip-head').text.replace('\n', '') # removing whitespace
+    tooltip_head_data = ['s'+str(id_counter), tooltip_head, datetime.now().strftime('%Y-%m-%d %H:%M:%S')] # pushing sid, name and date into the array
+    tooltip_values = div.select('td.tooltip-value') # selecting the tooltip values
+    # looping over the tooltip values
     for i, value in enumerate(tooltip_values):
         if i == 0:
-            tooltip_head_data.append('w')
-            trimmedValue = value.text.replace('\xa0', '')
+            tooltip_head_data.append('w') # if it's first index, then push 'w' as a param
+            trimmedValue = value.text.replace('\xa0', '') # clearing the value
             if trimmedValue.isdigit():
-                tooltip_head_data.append(trimmedValue)
-                tooltip_heads.append(tooltip_head_data)
-    id_counter += 1
+                tooltip_head_data.append(trimmedValue) # pushing only if it's not empty
+                tooltip_heads.append(tooltip_head_data) # pushing into the main array
+    id_counter += 1 # increasing the counter
  
-id_counter = 1
+id_counter = 1 # Resetting the counter
+# Repeating the same procedure to extract discharge values 'q'
 for div in tooltip_divs:
     tooltip_head = div.select_one('.tooltip-head').text.replace('\n', '')
     tooltip_head_data = ['s'+str(id_counter), tooltip_head, datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
@@ -48,13 +43,13 @@ for div in tooltip_divs:
                 tooltip_heads.append(tooltip_head_data)
     id_counter += 1
 
-filename = '/Users/aalshafi/GeoInformatics/finalassignment/periodic_data.csv'
-file_exists = os.path.isfile(filename)
+filename = '/Users/aalshafi/GeoInformatics/finalassignment/periodic_data.csv' # define the file name with the path
+file_exists = os.path.isfile(filename) # Check if the file exists
 
 # Write the data to a CSV file
 with open(filename, 'a', newline='') as csvfile:
     writer = csv.writer(csvfile)
     if not file_exists:  # Check if the file exists and if not, write the header row
-        writer.writerow(['sid', 'place', 'timestamp', 'param', 'value'])
+        writer.writerow(['sid', 'place', 'timestamp', 'param', 'value']) # write the csv header
     for item in tooltip_heads:
         writer.writerow(item)
--- cronscript2.py
+++ cronscript2.py
@@ -1,16 +1,17 @@
-
+#import necessary packages
 import pandas as pd
 import psycopg2
 import sqlalchemy
 import os
 
+# Connect with the database
 engine = sqlalchemy.create_engine("postgresql://env_master:M123xyz@localhost/groundwater")
 conn = engine.connect()
 #conn = psycopg2.connect(host="localhost", database="groundwater", user="env_master", password="M123xyz")
 
-df = pd.read_csv('periodic_data.csv')
+df = pd.read_csv('periodic_data.csv') # read the csv file
 
-df.to_sql('Waterlevel', con=conn, if_exists='append', index=False)
+df.to_sql('Waterlevel', con=conn, if_exists='append', index=False) # pushing to database
 
 # delete all rows except the header from the CSV file
 os.remove('periodic_data.csv')
